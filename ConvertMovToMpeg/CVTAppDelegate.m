21b5d6993559fd93bbbfe60352dcef7e11585113
questbeat <questbeat@gmail.com>
2014-02-01 18:06:16 +0900

    First commit

--- ConvertMovToMpeg/CVTAppDelegate.m
+++ ConvertMovToMpeg/CVTAppDelegate.m
@@ -7,12 +7,60 @@
 //
 
 #import "CVTAppDelegate.h"
+#import <AVFoundation/AVFoundation.h>
 
 @implementation CVTAppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+    NSOpenPanel *openPanel = [NSOpenPanel openPanel];
+    [openPanel setCanChooseDirectories:NO];
+    [openPanel setCanChooseFiles:YES];
+    [openPanel setAllowedFileTypes:@[@"mov"]];
+    
+    NSString *desktopPath = [NSSearchPathForDirectoriesInDomains(NSDesktopDirectory, NSUserDomainMask, YES) lastObject];
+    [openPanel setDirectoryURL:[NSURL fileURLWithPath:desktopPath]];
+    
+    [openPanel beginSheetModalForWindow:[NSApp keyWindow]
+                      completionHandler:^(NSInteger result) {
+                          if (result == NSOKButton) {
+                              NSString *selectedFilePath = [[[openPanel URLs] firstObject] path];
+                              
+                              // Convert mov to mp4
+                              NSString *outputFilePath = [[selectedFilePath stringByDeletingPathExtension] stringByAppendingPathExtension:@"mp4"];
+                              NSURL *outputURL = [NSURL fileURLWithPath:outputFilePath];
+                              
+                              AVAsset *asset = [AVAsset assetWithURL:[NSURL fileURLWithPath:selectedFilePath]];
+                              AVAssetExportSession *exportSession = [AVAssetExportSession exportSessionWithAsset:asset presetName:AVAssetExportPresetPassthrough];
+                              
+                              exportSession.outputURL = outputURL;
+                              exportSession.outputFileType = AVFileTypeMPEG4;
+                              exportSession.shouldOptimizeForNetworkUse = YES;
+                              
+                              [exportSession exportAsynchronouslyWithCompletionHandler:^{
+                                  switch ([exportSession status]) {
+                                      case AVAssetExportSessionStatusCompleted:
+                                          NSLog(@"Completed.");
+                                          break;
+                                          
+                                      case AVAssetExportSessionStatusFailed:
+                                          NSLog(@"Failed.");
+                                          break;
+                                          
+                                      case AVAssetExportSessionStatusCancelled:
+                                          NSLog(@"Cancelled.");
+                                          break;
+                                          
+                                      default:
+                                          break;
+                                  }
+                                  
+                                  [NSApp terminate:nil];
+                              }];
+                          } else {
+                              [NSApp terminate:nil];
+                          }
+                      }];
 }
 
 @end
